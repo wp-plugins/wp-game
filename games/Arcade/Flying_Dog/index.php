c681d84b7cfb752fc773eff4e59dbdf99d571095
mayxaybot <mayxaybot@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-12 00:27:54 +0000

    games
    git-svn-id: https://plugins.svn.wordpress.org/wp-game/trunk@1087648 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ games/Arcade/Flying_Dog/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <base href="<?php echo plugins_url() . '/wp-game/games/Arcade/Flying_Dog/' ?>">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <title>Flying Dog</title>
+
+    <script type="text/javascript" src="src/game/config.js"></script>
+    <script type="text/javascript" src="src/engine/core.js"></script>
+    <script type="text/javascript" src="src/game/main.js"></script>
+</head>
+<body>
+<?php wp_footer(); ?>
+</body>
+</html>
